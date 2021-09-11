a1bf4b918a4709474abc6dd72a708c5029c721d6
Mohasin-Haque <mohasinhaque23121@gmail.com>
2021-09-12 00:26:06 +0530

    added privacy

--- script.js
+++ script.js
@@ -19,22 +19,18 @@ function checkBirthdateIsLucky(){
     compareValues(sum, luckyNumber.value)
     else
     outputBox.innerText = "Please enter both the fields 🤷";
-    // console.log(sum);
 }
 
 
 function calculateSum(dob){
-    //replaceAll() use for replacing the specific things
     dob = dob.replaceAll("-","");
     let sum = 0;
     for (let index = 0; index < dob.length; index++){
-        sum = sum + Number(dob.charAt(index)); //since the dob was a string so we have typecasted it by Number
+        sum = sum + Number(dob.charAt(index));
     } 
 
     return sum;
 }
-
-// console.log(typeof dateOfBirth.value);
 checkNumberButton = addEventListener("click", checkBirthdateIsLucky)
 
 
